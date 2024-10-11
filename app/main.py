4311ccd04d975b1cebe280725cc6541f3d158fd1
T6nisValk <tnsvalk@gmail.com>
2024-10-11 06:53:14 +0300

    basic

---
+++ app/main.py
@@ -0,0 +1,33 @@
+import pygame
+import pygame.draw
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGTH = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+
+player = pygame.Rect((300, 250, 25, 25))
+
+run = True
+while run:
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (255, 0, 0), player)
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-1, 0)
+    elif key[pygame.K_d] == True:
+        player.move_ip(1, 0)
+    elif key[pygame.K_w] == True:
+        player.move_ip(0, -1)
+    elif key[pygame.K_s] == True:
+        player.move_ip(0, 1)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+pygame.quit()
